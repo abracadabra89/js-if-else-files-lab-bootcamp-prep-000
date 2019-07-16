072eed82cea440076e82de5c6e11a7b9653a46f5
anya litvinova <anya.s.litvinova@gmail.com>
2019-07-16 18:44:56 +0000

    Automatically backed up by Learn

--- tests.js
+++ tests.js
@@ -2,10 +2,10 @@ var index = require("./index.js")
 
 if (index.name === "Susan") {
     console.log("The name is correct")
+}
     if(index.height === 70){
       console.log('The height is correct')
-    }
-} else {
+    } else {
     console.log("Expected: Susan, Received: "+index.name)
 }
 
